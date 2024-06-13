1bfe50737b83a4d42fd002ca9a880f8b186e2dce
Your Name <you@example.com>
2024-06-14 00:03:08 +0700

    Add the PLL with HSE

---
+++ PLL_SYSCLK_HSE/Inc/main.h
@@ -0,0 +1,20 @@
+/*
+ * main.h
+ *
+ *  Created on: May 24, 2024
+ *      Author: thaithinhtran
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+
+#define SYS_CLOCK_FREQ_50MHZ	50
+#define SYS_CLOCK_FREQ_84MHZ	84
+#define SYS_CLOCK_FREQ_120MHZ	120
+#define SYS_CLOCK_FREQ_168MHZ	168
+
+
+#endif /* INC_MAIN_H_ */
---
+++ PLL_SYSCLK_HSE/Src/it.c
@@ -0,0 +1,17 @@
+/*
+ * it.c
+ *
+ *  Created on: May 24, 2024
+ *      Author: thaithinhtran
+ */
+#include "it.h"
+#include "main.h"
+
+extern UART_HandleTypeDef huart2;
+
+void SysTick_Handler (void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
