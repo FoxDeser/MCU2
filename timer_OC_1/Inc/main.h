714f8f50d984de447a3ec789f9b55bff2cc2c4f8
Your Name <you@example.com>
2024-08-11 21:13:00 +0700

    Add Output Compare Project

---
+++ timer_OC_1/Inc/main.h
@@ -0,0 +1,22 @@
+/*
+ * main.h
+ *
+ *  Created on: May 24, 2024
+ *      Author: thaithinhtran
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_conf.h"
+
+#define SYS_CLOCK_FREQ_50MHZ	50
+#define SYS_CLOCK_FREQ_84MHZ	84
+#define SYS_CLOCK_FREQ_120MHZ	120
+#define SYS_CLOCK_FREQ_168MHZ	168
+
+#define TRUE 1
+#define FALSE 0
+#endif /* INC_MAIN_H_ */
